dbf4782d4c1102cc9bbebb421516f06b233a10c0
Chirag Kular <ck4957@rit.edu>
2025-02-27 18:48:08 -0500

    Initial Commit

---
+++ VisionPro-MemoFlip/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  VisionPro-MemoFlip
+//
+//  Created by Chirag Kular on 2/27/25.
+//
+
+import SwiftUI
+import RealityKit
+import RealityKitContent
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Model3D(named: "Scene", bundle: realityKitContentBundle)
+                .padding(.bottom, 50)
+
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview(windowStyle: .automatic) {
+    ContentView()
+}
---
+++ VisionPro-MemoFlip/VisionPro_MemoFlipApp.swift
@@ -0,0 +1,17 @@
+//
+//  VisionPro_MemoFlipApp.swift
+//  VisionPro-MemoFlip
+//
+//  Created by Chirag Kular on 2/27/25.
+//
+
+import SwiftUI
+
+@main
+struct VisionPro_MemoFlipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
